a200c0dc39afee116e8b9c48d439daf213a9ab01
Your Name <yang.lin.99@bk.ru>
2023-08-06 20:46:30 +0300

    delete burger menu

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuBtn = document.querySelector('.menu-btn');
-    menuBtn.addEventListener('click', () => {
-        const headerList = document.querySelector('.header-menu__list');
-        headerList.classList.toggle('active');
-        menuBtn.classList.toggle('active');
-    });
-});
+document.addEventListener("DOMContentLoaded", () => {});
